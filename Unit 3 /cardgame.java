cd54dc68bddc0bf9404e07fda7a41fad7be729f8
hkooiman <101125724+hkooiman@users.noreply.github.com>
2024-11-07 14:16:45 +0000

    guess game new file

--- Unit 3 /cardgame.java
+++ Unit 3 /cardgame.java
@@ -1,9 +1,67 @@
+import java.util.Scanner;
 public class cardgame {
 
     public static void main(String[] args) 
   {
+    Scanner scan = new Scanner(System.in);
+    
+    int number = (int) (Math.random() * 100) + 1;
+    
+    System.out.println("Would you like to play a guessing game? (y/n)");
+
+    String response = scan.nextLine();
+
+    int plays = 0;
+    int totalGuesses = 0;
+
+    while (response.equals("y")){
+
+    plays++;
 
 
+    System.out.println("Choose a random number from 1 to 100");
+    
+    int guess = scan.nextInt();
+    
+    int numberOfGuesses = 0;
+
+
+    while (guess != number){
+      if (guess < number){
+
+        System.out.println("Too low! Try a higher number!");
+        guess = scan.nextInt();
+      }
+      else{
+        System.out.println("Too high! Try a lower number!");
+        guess = scan.nextInt();
+      }
+
+      numberOfGuesses++;
+
+
+    }
+    numberOfGuesses++;
+    System.out.println("You got it!");
+    if (numberOfGuesses >= 10){
+      System.out.println("Oh dear, you took " + numberOfGuesses + " tries! Try harder next time!");
+    }
+    else if (numberOfGuesses >= 5){
+      System.out.println("Good job! " + numberOfGuesses + " guesses is impressive!");
+    }
+    else{
+      System.out.println("Wow, you're a mindreader!");
+    }
+    
+    totalGuesses += numberOfGuesses;
+  }
+
+  int averageTries = totalGuesses/plays;
+  System.out.println("Average number of guesses = " + averageTries);
+
+
+
+   
 
 
 
