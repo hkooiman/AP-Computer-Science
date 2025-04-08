28db1c147392411d8b4228f6d8d7df2ab7a656a5
hkooiman <101125724+hkooiman@users.noreply.github.com>
2025-04-08 12:48:42 +0000

    new file

--- Polygon.java
+++ Polygon.java
@@ -1,4 +1,5 @@
 //example on inheretance
+import java.util.ArrayList;
 public class Polygon
 {
     private String shapeName;
@@ -28,6 +29,21 @@ public class Polygon
         return numSides;
     }
 
+    public String compareSides(Polygon p2)
+    {
+        if (this.numSides > p2.numSides)
+        {
+            return "A " + this.getName() + " has more sides than a " + p2.getName();
+        }
+        else if (this.numSides < p2.numSides)
+        {
+            return "A " + this.getName() + " has fewer sides than a " + p2.getName();
+        }
+        else
+        return "A " + this.getName() + " has the same number of sides as a " + p2.getName();
+
+    }
+
     public String toString()
     {
         return "Shape: " + shapeName + " Sides: " + numSides;
@@ -35,7 +51,7 @@ public class Polygon
 
     public static void main(String[] args)
     {
-         Polygon p1 = new Polygon(5, "Pentagon");
+       /*   Polygon p1 = new Polygon(5, "Pentagon");
          Triangle t1 = new Triangle(3,4,5);
          EquilateralTriangle t2 = new EquilateralTriangle(2);
          Square s1 = new Square();
@@ -43,6 +59,27 @@ public class Polygon
          System.out.println(t2.getName() + "-" + t2.getSides());
          System.out.println(t1);
          System.out.println(s1);
+
+         //Triangle p2 = new Polygon(4, "triangle"); -> polygon can't be a type of triangle, it only goes the other way
+
+         ArrayList<Polygon> shapes = new ArrayList<Polygon>();
+         shapes.add(p1);
+         shapes.add(t1);
+         shapes.add(t2);
+         shapes.add(s1); */
+
+         EquilateralTriangle t1 = new EquilateralTriangle(3);
+         Triangle t2 = new EquilateralTriangle(4);
+         Polygon t3 = new EquilateralTriangle(2);
+         System.out.println(t1);
+         System.out.println(t2);
+         System.out.println(t3);
+         Square s1 = new Square();
+         System.out.println(s1.compareSides(t1));
+         System.out.println(t1.compareSides(s1));
+
+
+         
     }
 }
 
@@ -75,6 +112,11 @@ class EquilateralTriangle extends Triangle
     public EquilateralTriangle(int s)
     {
         super(s,s,s);
+    }
+
+    public String toString()
+    {
+        return "EQ: " + super.toString();
     }
 
 }
